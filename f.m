091d3bded14beef1ee5baab635c70b63fc3ed28c
Lucas Axel Gamaleri <lucas.gamaleri.92@gmail.com>
2018-09-24 15:31:33 -0300

    Fix vinf

--- f.m
+++ f.m
@@ -22,8 +22,8 @@ Clp = -4.5 ;
 Clr = 0.5 ;
 Cnr = 0.38 ;
 Cnp = -0.8 ;
-vinf = sqrt(y(1)^2 + y(2)^2 + y(3)^2) ;
-
+%vinf = sqrt(y(1)^2 + y(2)^2 + y(3)^2) ;
+vinf = 180;
 xp(1)= ((0.5*rho*vinf^2*sup*(Cyb*y(1))+ m*g*y(4))/ m) ;
 xp(2)= (0.5*rho*vinf^2*sup*b*(Clb*y(1) +  (Clp*(y(3)*b)/(2*vinf)))/(2*Ix));
 xp(3)= 0.5*(rho*vinf^2*sup*b*(Cnr*((y(2)*y(1))/2*vinf) +  Cnp*(y(3)*b)/2*vinf))/Iz ;
